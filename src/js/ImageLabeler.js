ef1382303cf58ce4ec14bd6dd1d8218be60844c1
Benjamin Wu <benjaminwu124@gmail.com>
2018-09-17 00:06:38 -0400

    Load images using konva imstead of jquery and resize to window size

--- src/js/ImageLabeler.js
+++ src/js/ImageLabeler.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import $ from "jquery";
+import { Stage, Layer, Image } from 'react-konva';
 
 import '../css/ImageLabeler.css'
 
@@ -7,51 +7,85 @@ class ImageLabeler extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loadedImage: null,
+      image: null,
+      imageWidth: 0,
+      imageHeight: 0,
       isValidImage: false,
-      imageLoadListenerBound: false
-    }
+      containerWidth: 0,
+      containerHeight: 0
+    };
+
+    this.imageFrame = React.createRef();
+  }
+
+  componentDidMount() {
+    this.setState({
+      containerWidth: this.imageFrame.current.offsetWidth,
+      containerHeight: this.imageFrame.current.offsetHeight
+    }, () => {
+      console.log(`width: ${this.state.containerWidth} height: ${this.state.containerHeight}`);
+    });
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('update');
     if (prevProps.selectedFile !== this.props.selectedFile) {
-      const previewImage = $('#previewImage');
-      if (previewImage) {
-        if (!this.state.imageLoadListenerBound) {
-          previewImage.on('load', () => {
-            console.log(previewImage.height());
-            this.setState({
-              isValidImage: true
-            });
-          }).on('error', () => {
-            // TODO: Return error for invalid files
-            this.setState({
-              isValidImage: false
-            });
-          });
+      let image = new window.Image();
+      image.src = 'file://' + this.props.selectedFile;
+      image.onload = () => {
+        this.setState({
+          isValidImage: true,
+          image
+        }, () => {
           this.setState({
-            imageLoadListenerBound: true
+            imageWidth: this.state.image.width,
+            imageHeight: this.state.image.height,
+            containerWidth: this.imageFrame.current.offsetWidth,
+            containerHeight: this.imageFrame.current.offsetHeight
           });
-        }
-        if (this.props.selectedFile !== null) {
-          previewImage.attr('src', 'file://' + this.props.selectedFile);
-        }
-      }
+        });
+      };
     }
+  }
+
+  renderImage() {
+    const padding = 96;
+    const maxWidth = this.state.containerWidth - padding;
+    const maxHeight = this.state.containerHeight - padding;
+
+    const imageWidth = this.state.imageWidth;
+    const imageHeight = this.state.imageHeight;
+
+    const widthScale = imageWidth === 0 ? 1 : Math.min(imageWidth, maxWidth) / imageWidth;
+    const heightScale = imageHeight === 0 ? 1 : Math.min(imageHeight, maxHeight) / imageHeight;
+    const scale = Math.min(widthScale, heightScale);
+
+    const calculatedWidth = imageWidth * scale;
+    const calculatedHeight = imageHeight * scale;
+
+    return (
+      <div style={{width: calculatedWidth, height: calculatedHeight}}
+           className={"imageFrame " + (this.state.isValidImage ? '' : 'hiddenImage')}>
+        <Stage width={calculatedWidth}
+               height={calculatedHeight}>
+          <Layer>
+            <Image width={calculatedWidth}
+                   height={calculatedHeight}
+                   image={this.state.image}/>
+          </Layer>
+        </Stage>
+      </div>
+    )
   }
 
   render() {
     return (
-      <div className="toolView">
+      <div className="toolView" ref={this.imageFrame}>
 
         <div className="labelerHeader">
           {this.props.selectedFile || 'Placeholder'}
         </div>
 
-        <div className={"imageFrame " + (this.state.isValidImage ? '' : 'hiddenImage')}>
-          <img id="previewImage" alt=""/>
-        </div>
+        {this.renderImage()}
 
       </div>
     );
